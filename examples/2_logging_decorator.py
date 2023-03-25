c72ac64f8778389b09ed5890e289bd20fcf61475
Josh Schneider <josh.schneider@gmail.com>
2023-03-24 19:33:08 -0500

    Added examples.

---
+++ examples/2_logging_decorator.py
@@ -0,0 +1,42 @@
+import logging
+from functools import wraps
+
+
+LOG = logging.getLogger(__name__)
+
+
+def log_call(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        LOG.info("--> Calling %s", func.__name__)
+
+        try:
+            result = func(*args, **kwargs)
+
+        except:
+            LOG.error("<!- ERROR occurred calling %s", func.__name__, exc_info=True)
+            raise
+
+        else:
+            LOG.info("<-- %s returning %r", func.__name__, result)
+            return result
+
+    return wrapper
+
+
+@log_call
+def my_test_function(is_fail=False):
+    LOG.info("Inside my_test_function, where is_fail=%r", bool(is_fail))
+    if is_fail:
+        raise RuntimeError("I am a scary failure!")
+    return True
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname).1s %(funcName)s: %(message)s"
+    )
+
+    my_test_function()
+    my_test_function(0)
+    my_test_function(is_fail=True)
---
+++ examples/3_registration_decorator.py
@@ -0,0 +1,40 @@
+from collections import UserList
+
+
+class CategoryCollection(UserList):
+    def __init__(self, category):
+        super().__init__()
+        self.category = category
+
+    def __call__(self, func):
+        self.append(func)
+        return func
+
+    def call_all(self, *args, **kwargs):
+        yield from (cb(*args, *kwargs) for cb in self)
+
+
+actions = CategoryCollection("actions")
+
+
+@actions
+def double(i):
+    return i * 2
+
+
+@actions
+def half(i):
+    return i // 2
+
+
+@actions
+def add_three(i):
+    return i + 3
+
+
+if __name__ == "__main__":
+    actions(lambda i: i**2)
+
+    print(list(actions.call_all(3)))
+    print(list(actions.call_all(21)))
+    print(list(actions.call_all(99)))
